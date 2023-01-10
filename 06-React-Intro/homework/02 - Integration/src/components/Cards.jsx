66ad515ffb62c52739a79e5d532e3cb992beec69
Cecilia Castellano <emiriritey@gmail.com>
2023-01-10 07:40:53 -0300

    Homework 1 react intro

--- 06-React-Intro/homework/02 - Integration/src/components/Cards.jsx
+++ 06-React-Intro/homework/02 - Integration/src/components/Cards.jsx
@@ -2,5 +2,19 @@ import Card from './Card';
 
 export default function Cards(props) {
    const { characters } = props;
-   return <div></div>;
+   return (
+   <div>
+      {
+      characters.map(({name,species,gender,image})=>{
+         return <Card
+         name={name}
+         species={species}
+         gender={gender}
+         image={image}
+         onClose={()=>window.alert('Emulamos que se cierra la card')}
+         />
+      })
+      }
+   </div>
+   );
 }
